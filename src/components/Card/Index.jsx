0cb328ebf8e60c1a4071cfcbb9a438e87740acba
DokJS <omarmbengue6919@gmail.com>
2022-05-13 20:55:42 +0000

    Design : added Link's styles

--- src/components/Card/Index.jsx
+++ src/components/Card/Index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DefaultPicture from '../../assets/profile.png'
+import DefaultPicture from '../../assets/profile.png';
+import styled from 'styled-components';
+
+const CardLabel = styled.span`
+color: #5843e4;
+font-size:22px;
+font-weight:bold;
+`;
+const CardImage = styled.img`
+height:80px;
+width:80px;
+border-radius:50%;
+`
 
 const Card = (props) => {
    const {name,picture,jobTitle} = props.profile
   return (
     <div>
           <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <span>{jobTitle}</span>
-            <img src={picture} alt="freelance" height={80} width={80} />
+            <CardLabel>{jobTitle}</CardLabel>
+            <CardImage src={picture} alt="freelance" />
             <span>{name}</span>
         </div>
     </div>
--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../Utils/Style/colors';
+
+const StyledLink = styled(Link)`
+padding: 15px;
+color: #8186a0;
+text-decoration:none;
+font-size : 18px;
+${ (props)=>
+    props.$isFullLink && 
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`;
 
 const Header = () => {
   return (
     <nav>
-        <Link to={"/"}>Accueil</Link>
-        <Link to={"/survey/:questionNumber"}>Questionnaire</Link>
-        <Link to={"/results"}>Résultats</Link>
-        <Link to={"/freelances"}>Freelances</Link>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to={"/survey/1" } $isFullLink>Questionnaire</StyledLink>
+        <StyledLink to={"/freelances"}>Profils</StyledLink>
     </nav>
   )
 }
